fdb98fa49493ca5a4547b7b6b1faf8048e2cfc27
AnatoliiTolya <ilya.krupin69@gmail.com>
2022-03-18 14:31:11 +0700

    code

---
+++ fighting/Enites/Entity.cs
@@ -0,0 +1,84 @@
+﻿using System.Drawing;
+
+namespace fighting.Enites
+{
+    public  class Entity
+    {
+        public  int posX;
+        public  int posY;
+
+
+        public int dirX;
+        public int dirY;
+        public bool isMoving;
+
+
+        public int flip;
+
+        public int currentAnimation;
+        public int currentFrame;
+        public int currentLimit;
+        public int idleFrames;
+        public  int runFrames;
+        public  int attackFrames;
+        public  int deathFrames;
+
+        public int size;
+
+        public  Image spriteSheet;
+
+
+        public Entity(int posX, int posY, int idleFrames, int runFrames, int attackFrames, int deathFrames, Image spriteSheet)        //здесь функции для анимации персонажей и их перемещение по карте
+        {
+            this.posX = posX;
+            this.posY = posY;
+            this.idleFrames = idleFrames;
+            this.runFrames = runFrames;
+            this.attackFrames = attackFrames;
+            this.deathFrames = deathFrames;
+            this.spriteSheet = spriteSheet;
+            size = 31;
+            currentAnimation = 0;
+            currentFrame = 0;
+            currentLimit = idleFrames;
+            flip = 1;
+        }
+
+
+        public void Move()
+        {
+            posX += dirX;
+            posY += dirY;      
+        }
+
+
+        public void PlayAnimation(Graphics g)
+        {
+            g.DrawImage(spriteSheet, new Rectangle(new Point(posX - flip*size/2, posY), new Size(flip*size, size)), 32*currentFrame, 32*currentAnimation, size, size, GraphicsUnit.Pixel);
+            if (currentFrame < currentLimit - 1)
+                currentFrame++;
+            else currentFrame = 0;
+        }
+
+        public void SetAnimationConfiguration(int currentAnimation)        //состояния персонажа
+        {
+            this.currentAnimation = currentAnimation;
+            switch(currentAnimation)
+            {
+                case 0:
+                    currentLimit = idleFrames;
+                    break;
+                case 1:
+                    currentLimit = runFrames;
+                    break;
+                case 2:
+                    currentLimit = attackFrames;
+                    break;
+                case 3:
+                    currentLimit = deathFrames;
+                    break;
+            }
+        }
+
+    }
+}
---
+++ fighting/Form1.cs
@@ -0,0 +1,123 @@
+﻿using fighting.Enites;
+using fighting.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fighting
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+
+        public Entity player;
+        public Image dwarfSheet;
+        public Image gladiatorSheet;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 20;
+            timer1.Tick += new EventHandler(Update);
+
+            KeyDown += new KeyEventHandler(OnPress);
+            KeyUp += new KeyEventHandler(OnKeyUp);
+
+
+            Init();
+        }
+
+
+        public void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            player.dirX = 0;
+            player.dirY = 0;
+            player.isMoving = false;
+            player.SetAnimationConfiguration(0);
+        }
+
+
+
+        public void OnPress(object sender, KeyEventArgs e)                   //управление персонажем
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.W:
+                    player.dirY = -1;
+                    player.isMoving = true;
+                    player.SetAnimationConfiguration(1);
+                    break;
+                case Keys.S:
+                    player.dirY = 1;
+                    player.isMoving = true;
+                    player.SetAnimationConfiguration(1);
+                    break;
+                case Keys.A:
+                    player.dirX = -1;
+                    player.isMoving = true;
+                    player.flip = -1;
+                    player.SetAnimationConfiguration(1);
+                    break;
+                case Keys.D:
+                    player.dirX = 1;
+                    player.isMoving = true;
+                    player.flip = 1;
+                    player.SetAnimationConfiguration(1);
+                    break;
+                case Keys.Space:
+                    player.dirX = 0;
+                    player.dirY = 0;
+                    player.isMoving = false;
+                    player.SetAnimationConfiguration(2);
+                    break;
+            }
+            
+        }
+
+
+
+        public void Init()                   //отвечает за иниицаилизацию все игры
+        {
+            dwarfSheet = new Bitmap(Path.Combine (new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString(),"Sprites\\Dwarf.png"));
+
+
+             player = new Entity(100, 100, Hero.idleFrames, Hero.runFrames, Hero.attackFrames, Hero.deathFrames, dwarfSheet);
+            timer1.Start();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void Update(object sender, EventArgs e)
+        {
+            if (player.isMoving)
+                player.Move();
+            Invalidate();
+        }
+
+
+
+        private void OnePaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            player.PlayAnimation(g);
+
+        }
+    }
+}
